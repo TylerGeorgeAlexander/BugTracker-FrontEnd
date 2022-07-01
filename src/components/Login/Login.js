5f36ff7d48f2d228cdaa5c7e55d15fb9fd858bf8
Tyler G Alexander <tylergeorgealexander@gmail.com>
2022-06-30 17:01:43 -0700

    added logo to login page with proper alt desc

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import "./Login.css";
 import { Form, Button } from "react-bootstrap";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 async function loginUser(credentials) {
   const url = new URL(`${API_BASE_URL}/login`);
@@ -32,7 +33,12 @@ export default function Login({ setToken }) {
 
   return (
     <div className="login-wrapper">
-      <h1>Please Log In</h1>
+      <img
+        src="bugtracker.png"
+        alt="Bug Tracker Logo, which shows a bug within the scope of a magnifying glass."
+      />
+      <h1>BugTracker</h1>
+      <h2>Please Log In</h2>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
@@ -58,28 +64,6 @@ export default function Login({ setToken }) {
           Submit
         </Button>
       </Form>
-      <p>
-        <br />
-        This is a template login page. You could enter any character combination
-        for a username and password to receive a token that directs to the next
-        page.
-        <br />
-        <br />
-        Currently the template is programmed for <strong>sessionStorage</strong>
-        .
-        <br />
-        <br />
-        Changing between sessionStorage and localStorage allows different
-        benefits, such as localStorage's ability to save data across windows.
-        Unlike sessionStorage, localStorage will save data even after the
-        session ends. This can be more convenient, since it lets users open
-        multiple windows and tabs without a new login, but it does have some
-        security problems. If the user shares their computer, they will remain
-        logged in to the application even though they close the browser. It will
-        be the user’s responsibility to explicitly log out. The next user would
-        have immediate access to the application without a login. It’s a risk,
-        but the convenience may be worth it for some applications.
-      </p>
     </div>
   );
 }
